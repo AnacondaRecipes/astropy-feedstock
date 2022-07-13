257b76a339575127ebf3bd1301e026dda4d3af38
Serhii Kupriienko <skupriienko@anaconda.com>
2022-07-13 13:27:17 +0000

    Fix test

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,9 +1,11 @@
+import platform
+
 # import astropy._compiler
-import astropy.cosmology.scalar_inv_efuncs
+import astropy.cosmology.flrw.scalar_inv_efuncs
 import astropy.io.ascii.cparser
 import astropy.io.fits.compression
 import astropy.io.votable.tablewriter
-import astropy.modeling._projections
+import astropy.modeling.projections
 import astropy.timeseries.periodograms.lombscargle.implementations.cython_impl
 import astropy.table._column_mixins
 import astropy.table._np_utils
@@ -22,6 +24,11 @@ from astropy import test
 # They take more than two hours to run on some platforms!
 # test(package='io.ascii')
 
-test(package='time')
-test(package='wcs')
-test(package='convolution')
+if ((platform.machine() == 'aarch64') and
+    (platform.python_implementation().lower() != 'cpython')):
+    print('WARNING: Skipping tests on aarch64/PyPy because they take too long')
+
+else:
+    test(package='time')
+    test(package='wcs')
+    test(package='convolution')
