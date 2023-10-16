8dd9d39a37cf5a4c9b812ffb33f7806c787920a3
Serhii Kupriienko <79282962+skupr-anaconda@users.noreply.github.com>
2023-10-16 18:07:45 +0300

    astropy_5.3.4 (#16)

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -3,14 +3,12 @@ import platform
 # import astropy._compiler
 import astropy.cosmology.flrw.scalar_inv_efuncs
 import astropy.io.ascii.cparser
-import astropy.io.fits.compression
 import astropy.io.votable.tablewriter
 import astropy.modeling.projections
 import astropy.timeseries.periodograms.lombscargle.implementations.cython_impl
 import astropy.table._column_mixins
 import astropy.table._np_utils
-import astropy.utils._compiler
-import astropy.utils.xml._iterparser
+import astropy.utils.xml.iterparser
 import astropy.wcs._wcs
 
 # We run a subset of the tests which are the most likely to have
